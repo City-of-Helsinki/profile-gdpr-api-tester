41ba1abcdb9a782b8e68bf400fb33bd7aa50072b
Mikko Keskinen <mikko.keskinen@hel.fi>
2023-09-13 07:58:08 +0300

    Add a few comments

--- gdpr_api_tester/routes.py
+++ gdpr_api_tester/routes.py
@@ -7,6 +7,14 @@ routes = web.RouteTableDef()
 
 @routes.get("/{path:.*/?}.well-known/openid-configuration")
 async def handle_openid_configuration(request):
+    """OpenID configuration view
+
+    The path part in the route is not used. It's just added to make the route
+    match everything that ends in ".well-known/openid-configuration" to support
+    ISSUER values with path parts.
+
+    Only issuer and jwks_uri configuration values are returned as they are the only
+    ones that are needed when validating tokens."""
     jwks_uri = (
         app_config.ISSUER
         + ("/" if not app_config.ISSUER.endswith("/") else "")
@@ -23,9 +31,14 @@ async def handle_openid_configuration(request):
 
 @routes.get("/{path:.*/?}jwks")
 async def handle_jwks(request):
+    """JSON Web Key Set view
+
+    The path part in the route is not used. It's just added to make the route
+    match everything that ends in "jwks" to support ISSUER values with path parts."""
     from .rsa_key import kid, rsa_key
 
     key_data = rsa_key.public_key().to_dict()
+    # Add key id manually as RSAKey doesn't support key ids
     key_data.update(
         {
             "kid": kid,
--- gdpr_api_tester/rsa_key.py
+++ gdpr_api_tester/rsa_key.py
@@ -1,9 +1,15 @@
 import rsa
 from jose import jwk
+from jose.backends.base import Key
 from jose.constants import ALGORITHMS
 
 
-def _get_or_create_rsa_key():
+def _get_or_create_rsa_key() -> Key:
+    """Gets or creates an RSA key
+
+    The generated RSA key is saved in PEM format to the current directory in a file
+    named "gdpr_api_tester_key.pem".
+    """
     key = None
     print(
         "Trying to load RSA key from file gdpr_api_tester_key.pem...",
@@ -26,6 +32,8 @@ def _get_or_create_rsa_key():
             end="",
             flush=True,
         )
+        # Pubkey isn't saved as the public key can be derived from the private key
+        # when needed.
         (pubkey, privkey) = rsa.newkeys(2048)
         try:
             with open("gdpr_api_tester_key.pem", "wb") as f:
@@ -40,5 +48,8 @@ def _get_or_create_rsa_key():
     return key
 
 
+# kid and rsa_keys are effectively singletons as Python doesn't run the code multiple
+# times when importing.
+# Key id is set here so that the JWT encoding and jwks view can easily use the same name
 kid = "gdpr-api-tester-key"
 rsa_key = _get_or_create_rsa_key()
